0d41d27d2ed8058787c85f89cec63ccca6b4edd8
Tony Padilla <vainehertz@yahoo.com>
2022-05-16 09:53:05 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+class Plate extends React.Component {
+  constructor(props){
+    super(props)
+
+  }
+  render(){
+    return <div id='plate'>Alyssa's website <a href='https://www.instagram.com/meowcarons.pawtisserie/?hl=en'>donate here</a></div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Plate />
     </div>
   );
 }
