2f3fe3270e7dc215f9d4388c92cfadeac39f3dfe
Tony Padilla <vainehertz@yahoo.com>
2022-05-17 12:04:14 -0700

    added movement for macaron and catcher

--- src/App.js
+++ src/App.js
@@ -1,20 +1,78 @@
-import logo from './logo.svg';
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import catcher from './asset/catcher.png'
+import macaron from './asset/macaron.jpg'
+
+class Macarons extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      y: 0
+    }
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState((s, p) => {
+        return {y: this.state.y += 5}
+      })
+    }, 300)
+  }
+
+  render() {
+    let macaronStyle = {top: `${this.state.y}px`, left: window.innerWidth - window.innerWidth / 2}
+    return <div className='macaron' style={macaronStyle}><img src={macaron} style={{width: '30%'}}/></div>
+  }
+}
 
 class Plate extends React.Component {
   constructor(props){
     super(props)
 
+    this.state = {
+      x: window.innerWidth - window.innerWidth / 2,
+      y: window.innerHeight - window.innerHeight / 4
+    }
   }
+
+  componentDidMount() {
+    const plate = document.getElementById('plate')
+    window.addEventListener('resize', () => {
+      this.setState((s, p) => {
+        return {y: window.innerHeight - window.innerHeight / 4}
+      })
+    })
+    window.addEventListener('keydown', e => {
+      console.log(`${e.code} pressed x: ${this.state.x}`)
+      if (e.code == 'ArrowLeft'){
+        this.setState((s, p) => {
+          return {x: s.x - 3}
+        })
+      }
+      if (e.code == 'ArrowRight'){
+        this.setState((s, p) => {
+          return {x: s.x + 3}
+        })
+      }
+    })
+  }
+
   render(){
-    return <div id='plate'>Alyssa's website <a href='https://www.instagram.com/meowcarons.pawtisserie/?hl=en'>donate here</a></div>
+    let plateStyle = {top: this.state.y, left: this.state.x}
+    let catchStyle = {marginTop: '20px', width: '100%', position: 'absolute'}
+    return <div style={plateStyle} id='plate'>
+      <a href='https://www.instagram.com/meowcarons.pawtisserie/?hl=en'>
+        donate here
+      </a>
+      <img style={catchStyle} src={catcher} />
+    </div>
   }
 }
 
 function App() {
   return (
     <div className="App">
+      <Macarons />
       <Plate />
     </div>
   );
