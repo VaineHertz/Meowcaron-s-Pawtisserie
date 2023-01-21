15d37dcb2628efcdb8376f06e3074cc660330933
Tony Padilla <vainehertz@yahoo.com>
2023-01-21 13:31:16 -0800

    updates

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import './index.scss'
 import Sidebar from '../Sidebar'
-import InstagramEmbed from 'react-instagram-embed'
 import chef from '../../asset/chef.png'
 
 class Layout extends React.Component {
@@ -12,10 +11,12 @@ class Layout extends React.Component {
       return <>
       <Sidebar />
       <div className='landing'>
-        <h1>
-        Hello my name is Aly and I am a professional Macaronettear
-        </h1><br/><br/>
-        Please trust me with your macaron pastry needs!
+        <div className='landing-text'>  
+          <h1>Meowcarons</h1>
+          <h2>Handmade Macarons</h2>
+          <h3>Delivered to your door</h3>   
+          </div>
+          <img className='chef' src={chef}></img> 
       </div>
       </>
     }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,23 +3,20 @@ import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import logo from '../../asset/logo.png'
-import contact from '../../asset/contact.png'
-import gamepad from '../../asset/gamepad.png'
-import macaron from '../../asset/macaron.jpg'
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img className='logo' src={logo}></img>
         </Link>
-        <Link className='orderLink' to='/order'>
-            <img className='macaron' src={macaron}></img>
+        <Link className='macaron' to='/order'>
+            Order
         </Link>
-        <Link className='contactLink' to='/contact'>
-            <img className='contact' src={contact}></img>
+        <Link className='contact' to='/contact'>
+            Contact
         </Link>
-        <Link className='gameLink' to='/game'>
-            <img className='gamepad' src={gamepad}></img>
+        <Link className='gamepad' to='/game'>
+            Game
         </Link>
         <a target='_blank' className='instagram' href='https://www.instagram.com/meowcarons.pawtisserie/?hl=en'>
             <FontAwesomeIcon icon={faInstagram} />
