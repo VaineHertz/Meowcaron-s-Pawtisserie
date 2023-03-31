4e3c0fdee7c5de027d774de25b45870830531e26
Tony Padilla <vainehertz@yahoo.com>
2023-03-31 13:59:56 -0700

    fixed navbar with gpt3.5'

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,27 +1,33 @@
-import { Link } from 'react-router-dom'
-import './index.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import logo from '../../asset/logo.png'
+import { Link } from 'react-router-dom';
+import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import logo from '../../asset/logo.png';
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img className='logo' src={logo}></img>
-        </Link>
-        <Link className='macaron' to='/order'>
-            Order
-        </Link>
-        <Link className='contact' to='/contact'>
-            Contact
-        </Link>
-        <Link className='gamepad' to='/game'>
-            Game
-        </Link>
-        {/* <a target='_blank' className='instagram' href='https://www.instagram.com/meowcarons.pawtisserie/?hl=en'>
-            <FontAwesomeIcon icon={faInstagram} />
-        </a> */}
+  <div className="nav-bar">
+    <Link to="/" className="logo-link">
+      <img className="logo-image" src={logo} alt="Logo" />
+    </Link>
+    <div className="links-container">
+      <Link className="nav-link" to="/order">
+        Order
+      </Link>
+      <Link className="nav-link" to="/contact">
+        Contact
+      </Link>
+      <Link className="nav-link" to="/game">
+        Game
+      </Link>
     </div>
-)
+    <a
+      target="_blank"
+      className="instagram-link"
+      href="https://www.instagram.com/meowcarons.pawtisserie/?hl=en"
+    >
+      <FontAwesomeIcon icon={faInstagram} />
+    </a>
+  </div>
+);
 
-export default Sidebar
+export default Sidebar;
